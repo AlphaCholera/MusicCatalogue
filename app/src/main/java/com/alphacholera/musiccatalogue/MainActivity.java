bcfceda03979f9a3f795845b406b69e43bd50893
Utkarsh Pankaj <alphacholera0201@gmail.com>
2019-04-14 01:56:52 +0530

    Replaced TabLayout with BottomNavigationView and enhanced UI.

--- app/src/main/java/com/alphacholera/musiccatalogue/MainActivity.java
+++ app/src/main/java/com/alphacholera/musiccatalogue/MainActivity.java
@@ -1,9 +1,15 @@
 package com.alphacholera.musiccatalogue;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.alphacholera.musiccatalogue.HistoryFragment.HistoryFragment;
 import com.alphacholera.musiccatalogue.Fragments.ProfileFragment;
@@ -12,33 +18,57 @@ import com.alphacholera.musiccatalogue.Fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBar actionBar;
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int[] icons = {R.drawable.tab_home,
-                R.drawable.tab_trending,
-                R.drawable.tab_history,
-                R.drawable.tab_profile
-        };
-
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        setUpViewPager(viewPager);
-        tabLayout.setupWithViewPager(viewPager);
-        tabLayout.getTabAt(0).select();
-        for (int i = 0; i < icons.length; i++) {
-            tabLayout.getTabAt(i).setIcon(icons[i]);
-        }
+        navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        actionBar = getSupportActionBar();
+        loadFragment(new HomeFragment());
+        actionBar.setTitle("Home");
     }
-    void setUpViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.insertNewFragment(new HomeFragment());
-        adapter.insertNewFragment(new TrendingFragment());
-        adapter.insertNewFragment(new HistoryFragment());
-        adapter.insertNewFragment(new ProfileFragment());
-        viewPager.setAdapter(adapter);
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment;
+            switch (menuItem.getItemId()) {
+                case R.id.home:
+                    actionBar.setTitle("Home");
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.trending:
+                    actionBar.setTitle("Trending");
+                    fragment = new TrendingFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.history:
+                    actionBar.setTitle("History");
+                    fragment = new HistoryFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.profile:
+                    actionBar.setTitle("Trending");
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+//        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
